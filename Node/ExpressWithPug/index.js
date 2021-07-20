73d4d646a72657325147884eedd84df79f45b2df
Leonardo Contreras <LContreras@student.neumont.edu>
2021-07-20 08:47:13 -0600

    Added hello page and styling

--- Node/ExpressWithPug/index.js
+++ Node/ExpressWithPug/index.js
@@ -3,6 +3,7 @@ const pug = require('pug');
 const path = require('path');
 const routes = require("./routes/routes");
 
+
 const app = express();
 
 app.set('view engine', 'pug');
@@ -12,5 +13,6 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', routes.index);
 app.get('/potato', routes.potato);
+app.get('/hello/:name', routes.hello);
 
 app.listen(3000);
--- Node/ExpressWithPug/routes/routes.js
+++ Node/ExpressWithPug/routes/routes.js
@@ -1,11 +1,22 @@
+const config = require('../config.json');
+
 exports.index = (req, res) =>{
     res.render('index', {
-        title: 'Home'
+        title: 'Home',
+        config
     });
 };
 
 exports.potato = (req, res) =>{
     res.render('potato', {
-        title: 'PotatoLand'
+        title: 'PotatoLand',
+        config
+    });
+};
+
+exports.hello = (req, res) =>{
+    res.render('hello', {
+        title: `${req.params.name}'s Webpage`,
+        config
     });
 };
