847abccaeb2965a72265f1c9230b2080bb664124
Leonardo Contreras <LContreras@student.neumont.edu>
2021-07-22 08:46:46 -0600

    File System

--- Node/ExpressForms/routes/routes.js
+++ Node/ExpressForms/routes/routes.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 exports.index = (req, res) =>{
         res.render('index', {
             title: "Home"
@@ -8,8 +10,25 @@ exports.submitted = (req, res) =>{
     let person = {
         name: req.body.name,
         age: req.body.age,
-        species: req.body.species
+        species: req.body.species,
+        imgInput: req.body.imgInput
     };
+
+    let personData = `
+    <p>
+    name: ${person.name}<br />
+    age: ${person.age}<br />
+    species: ${person.species}<br />
+    image: <br /> 
+    <image src=${person.imgInput} width=250><br />
+    -----------------------------------------
+    </p>
+    `;
+
+    fs.writeFile('public/mytest.html', personData, err=>{
+        if(err) throw err;
+        console.log("DataSaved");
+    });
 
     res.render('submitted', {
         title: "Form Accepted",
